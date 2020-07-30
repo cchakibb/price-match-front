39039f2c7bd859a4d84fdf742030af6ee86039b5
cchakibb <chakibbachir88@gmail.com>
2020-07-30 18:33:28 +0200

    update pack.json App.js Calendar.js Dashboard.js

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
-
+import Calendar from "../components/Calendar";
 class Dashboard extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Calendar />
+      </div>
+    );
   }
 }
 
