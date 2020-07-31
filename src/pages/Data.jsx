0f3a51c0e2fc5e7cfefdcab55b9152d911052482
cchakibb <chakibbachir88@gmail.com>
2020-07-31 18:27:27 +0200

    debug Data.js chelou

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
 import apiHotel from "../api/apiHotel";
 import axios from "axios";
-​
+
 class Data extends Component {
   state = {
     hotels: [],
   };
-​
+
   componentDidMount() {
     apiHotel.getHotelInfo().then((data) => {
       const hotels = data.filter((d) => d !== null);
       this.setState({ hotels: hotels });
       axios
-        .post("http://localhost:4000/hotelData/data", {
-          hotels,
-        })
+        .post("http://localhost:4000/hotelData/data", { hotels })
         .then((DbRes) => console.log(DbRes))
         .catch((error) => console.log(error));
     });
   }
-​
   render() {
     console.log(this.state.hotels);
     return (
@@ -29,19 +26,5 @@ class Data extends Component {
     );
   }
 }
-​
+
 export default Data;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
