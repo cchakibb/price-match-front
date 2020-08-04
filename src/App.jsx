29dade144f1a7b9922748e8c074a9ef31c619ef7
cchakibb <chakibbachir88@gmail.com>
2020-08-04 17:16:01 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NavMain from "./components/NavMain";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-import FooterMain from './components/FooterMain';
+import FooterMain from "./components/FooterMain";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Profile from "./pages/Profile";
 import Dashboard from "./pages/Dashboard";
@@ -13,24 +13,23 @@ import ProfileUpdate from "./pages/ProfileUpdate";
 import Data from "./pages/Data";
 import Database from "./pages/Database";
 
-
 function App() {
   return (
     <div className="App">
       <NavMain />
-        <div className="content">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signin" component={Signin} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/data" component={Data} />
-        <Route exact path="/profile/settings" component={ProfileUpdate} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/test" component={Test} />
-        <Route exact path="/db" component={Database} />
+      <div className="content">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/data" component={Data} />
+          <ProtectedRoute exact path="/profile/settings" component={ProfileUpdate} />
+          <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/test" component={Test} />
+          <Route exact path="/db" component={Database} />
 
-        <ProtectedRoute exact path="/profile" component={Profile} />
-      </Switch>
+          <ProtectedRoute exact path="/profile" component={Profile} />
+        </Switch>
       </div>
       <FooterMain />
     </div>
